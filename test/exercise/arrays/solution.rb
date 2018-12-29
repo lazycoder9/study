7d4a304ffd965f28f7e50d2d6f5785cbf39b8418
Ulugbek Tuychiev <t.ulugbek.9@gmail.com>
2018-12-30 01:28:46 +0500

    solve arrays

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,11 +2,20 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+        array.map { |x| x.negative? ? x : array.max }
       end
 
-      def search(_array, _query)
-        0
+      def search(array, query)
+        iter = lambda do |left, right|
+          return -1 if left > right
+
+          mid = (left + right) / 2
+          return mid if array[mid] == query
+
+          new_params = array[mid] > query ? [left, mid - 1] : [mid + 1, right]
+          iter.call(*new_params)
+        end
+        iter.call(0, array.length - 1)
       end
     end
   end
