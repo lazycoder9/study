9ae8ca1c2a8b30354a9ac1f281d1f1a6e4686c25
Ulugbek Tuychiev <t.ulugbek.9@gmail.com>
2019-01-19 22:06:33 +0500

    fix linter warnings

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,7 +2,7 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        max_number = array.reduce(array[0]) { |acc, n| n > acc ? n : acc }
+        max_number = array.reduce(array[0]) { |a, e| e > a ? e : a }
         array.map { |x| x.negative? ? x : max_number }
       end
 
@@ -10,7 +10,7 @@ module Exercise
         left = 0
         right = array.length - 1
 
-        while left <= right do
+        while left <= right
           mid = (left + right) / 2
           return mid if array[mid] == query
           left, right = array[mid] > query ? [left, mid - 1] : [mid + 1, right]
